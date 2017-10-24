ad86a69ae90a7e46c45defa04c06e2a347c4880a
Cat Tran <trandinhcat@gmail.com>
2017-10-24 12:54:21 -0500

    added average and max filter

--- api/filters.py
+++ api/filters.py
@@ -77,3 +77,29 @@ def low_average_filter(dataframe=None, columns=None, threshold=100):
     return new_df
 
 
+def average_and_max_filter(dataframe=None, columns=None, avg_threshold=100, max_threshold=300):
+    """
+    This method accepts a pandas Dataframe that contains numeric values in its rows. It will perform 2 tests, an average
+    test and a max test. The average test is where the average for each row is calculated, if this average is not greater
+    or equal to the avg_threshold, that row fails the test. The max test is where the max value of each row is located, if
+    the max value of each row is not greater or equal to the max_threshold, that row fails the test. If a row fails BOTH
+    tests, it will be filtered out. The returned dataframe only contains the rows that do not fail BOTH test, failing one
+    test but passing the other is fine. The original dataframe will be unmodified.
+    :param dataframe: A pandas Dataframe that contains numeric values.
+    :param columns: A list of columns name in which the tests will be performed.
+    :param avg_threshold: A threshold for the average test.
+    :param max_threshold: A threshold for the max test.
+    :return: A new pandas Dataframe.
+    """
+    if dataframe is None:
+        raise ValueError("The 'dataframe' parameter containing numeric value to be filtered must be specified!")
+
+    new_df = dataframe.copy()
+    # if columns is not specified, all columns in the dataframe are considered
+    if columns is None:
+        new_df = new_df.loc[(new_df.mean(axis=1) >= avg_threshold) | (new_df.max(axis=1) >= max_threshold)]
+    else:
+        new_df = new_df.loc[(new_df[columns].mean(axis=1) >= avg_threshold) | (new_df[columns].max(axis=1) >= max_threshold)]
+    return new_df
+
+
